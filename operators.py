7a96f147607ffea3c53302688eb6df9474e703e7
georgekiroto <78143780+georgekiroto@users.noreply.github.com>
2021-03-08 08:28:11 +0300

    operators in python

---
+++ operators.py
@@ -0,0 +1,60 @@
+print('Hello, world!')
+#examples of relational operators
+a = 10
+b = 30
+# a > b is false
+print(a > b)
+# a < b is true
+print (a < b)
+# a == b is false
+print (a == b)
+#a != b is true
+print(a !=b)
+#a >= b is false
+print (a>=b)
+#a <=b is true
+print(a<=b)
+++++++++++++++++++++
+#examples of logical operators
+a = "true"
+b = "false"
+
+#print a and b is false
+print(a and b)
+
+#print a or b is true
+print(a or b)
+
+#print not a is false
+print(not a)
+++++++++++++++++++
+#examples of bitwise operators
+a = 10
+b = 5
+#print bitwise AND operation
+print(a & b)
+#print bitwise OR operation
+print(a | b)
+#print bitwise NOT operation
+print(~a)
+#print bitwise XOR operation
+print(a ^ b)
+#print bitwise right shift operation
+print(a >> 2)
+#print bitwise left shift operation
+print(a << 2)
+++++++++++++++++++++++
+a1 = 3
+b1 = 3
+a2 = 'programming in python'
+b2 = 'programming in python'
+a3 = [1,2,3]
+b3 = [1,2,3]
+
+print(a1 is not b1)
+
+print(a2 is b2)
+ 
+ #output is false, since lists are mutable.
+print(a3 is b3)
+ 
